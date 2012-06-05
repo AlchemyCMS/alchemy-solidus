f875ea14b44819330e190de2b000d6dca8c1d3a8
Thomas von Deyen <tvd@magiclabs.de>
2012-06-05 21:07:21 +0200

    Fixes for spree-1.2 beta

--- lib/alchemy_spree/engine.rb
+++ lib/alchemy_spree/engine.rb
@@ -5,7 +5,7 @@ module AlchemySpree
   class Engine < ::Rails::Engine
     engine_name 'alchemy_spree'
 
-    initializer 'spree.user_class' do
+    initializer 'spree.user_class', :after => 'alchemy.include_authentication_helpers' do
       Spree.user_class = "Alchemy::User"
       require File.join(File.dirname(__FILE__), '../spree/authentication_helpers')
     end
