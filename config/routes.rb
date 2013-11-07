286b386b59b93f231677d8f0afa6e263c1ef2fcd
Thomas von Deyen <tvdeyen@gmail.com>
2013-11-07 22:45:29 +0100

    Fixes routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
-Alchemy::Engine.routes.append do
-	get 'admin/spree' => 'alchemy/admin/spree#index', :as => :spree_admin
+Alchemy::Engine.routes.prepend do
+  get '/admin/spree' => 'admin/spree#index', :as => :spree_admin
 end
