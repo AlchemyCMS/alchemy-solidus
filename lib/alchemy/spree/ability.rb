dbacccb579b47e60e7386d3412ff06d0cfda061b
Thomas von Deyen <tvd@magiclabs.de>
2015-01-30 12:35:50 +0100

    Do not rely on user.admin? method
    We use the alchemy_roles method instead. This is already documented and reduces amount of methods the user class has to offer.

--- lib/alchemy/spree/ability.rb
+++ lib/alchemy/spree/ability.rb
@@ -4,11 +4,10 @@ module Alchemy
       include CanCan::Ability
 
       def initialize(user)
-        if user && user.admin?
+        if user && user.alchemy_roles.include?('admin')
           can :index, :alchemy_admin_spree
         end
       end
-
     end
   end
 end
