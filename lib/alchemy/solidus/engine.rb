a080351b0bed7ad92163626f9943a2f10b6b1ccb
Thomas von Deyen <tvd@bitspire.de>
2016-05-17 22:29:09 +0200

    Add alchemy_module_icon to precompiled assets

--- lib/alchemy/solidus/engine.rb
+++ lib/alchemy/solidus/engine.rb
@@ -3,6 +3,12 @@ module Alchemy
     class Engine < ::Rails::Engine
       engine_name 'alchemy_solidus'
 
+      initializer 'alchemy_solidus.assets' do |app|
+        app.config.assets.precompile += [
+          'alchemy/solidus/alchemy_module_icon.png'
+        ]
+      end
+
       def self.activate
         Alchemy.register_ability(::Spree::Ability)
       end
