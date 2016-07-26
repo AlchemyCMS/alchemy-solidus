2c02171a05176ad2a6940b5911ce7b0cd1c5733a
Marino Bonetti <marinobonetti@gmail.com>
2016-07-26 16:29:40 +0200

    Better integration
    For use Alchemy with multi-language we need the helpers contained in Alchemy::ConfigurationMethods like prefix_locale?

--- lib/alchemy/solidus/alchemy_in_solidus.rb
+++ lib/alchemy/solidus/alchemy_in_solidus.rb
@@ -1,3 +1,4 @@
 # Allow Alchemy content within Solidus views
 Spree::BaseController.send :include, Alchemy::ControllerActions
 Spree::UserSessionsController.send :include, Alchemy::ControllerActions if defined? Spree::UserSessionsController
+Spree::BaseController.send :include, Alchemy::ConfigurationMethods
