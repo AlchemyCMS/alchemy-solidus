18281eb4630bc95790dde59d1455470357910509
Thomas von Deyen <thomas@vondeyen.com>
2017-11-14 21:27:32 +0100

    Include Spree::UserMethods in Alchemy::User extension
    Without that the Alchemy::User is no proper Spree.user_class

--- lib/alchemy/solidus/alchemy_user_extension.rb
+++ lib/alchemy/solidus/alchemy_user_extension.rb
@@ -1,6 +1,10 @@
 module Alchemy
   module Solidus
     module AlchemyUserExtension
+      def self.included(klass)
+        klass.include Spree::UserMethods
+      end
+
       def spree_roles
         if admin?
           ::Spree::Role.where(name: 'admin')
