f3315a7edafd2a664930a49004eb38007f62bfd3
Thomas von Deyen <thomas@vondeyen.com>
2017-11-14 22:54:38 +0100

    Add Dummy Rails 5.1 app

---
+++ spec/dummy/config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
+Bundler.require(*Rails.groups)
+require "alchemy-solidus"
+
+module Dummy
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
+
