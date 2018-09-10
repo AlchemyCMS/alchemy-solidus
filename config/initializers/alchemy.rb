b476d85c92083417651bb32cd112954c204bdc06
Thomas von Deyen <thomas@vondeyen.com>
2018-09-10 11:44:31 +0200

    Add subnavigation to alchemy solidus tab

--- config/initializers/alchemy.rb
+++ config/initializers/alchemy.rb
@@ -1,10 +1,49 @@
-Alchemy::Modules.register_module({
+alchemy_module = {
   engine_name: 'spree',
   name: 'solidus',
   navigation: {
     controller: 'spree/admin/orders',
     action: 'index',
-    name: 'Shop',
-    image: 'alchemy/solidus/alchemy_module_icon.png'
+    name: 'Store',
+    image: 'alchemy/solidus/alchemy_module_icon.png',
+    sub_navigation: [
+      {
+        controller: 'spree/admin/orders',
+        action: 'index',
+        name: 'Orders'
+      },
+      {
+        controller: 'spree/admin/products',
+        action: 'index',
+        name: 'Products'
+      },
+      {
+        controller: 'spree/admin/reports',
+        action: 'index',
+        name: 'Reports'
+      },
+      {
+        controller: 'spree/admin/promotions',
+        action: 'index',
+        name: 'Promotions'
+      },
+      {
+        controller: 'spree/admin/stock_items',
+        action: 'index',
+        name: 'Stock'
+      }
+    ]
   }
-})
+}
+
+if defined?(Spree::Auth::Engine)
+  alchemy_module[:navigation][:sub_navigation].push(
+    {
+      controller: 'spree/admin/users',
+      action: 'index',
+      name: 'Users'
+    }
+  )
+end
+
+Alchemy::Modules.register_module(alchemy_module)
