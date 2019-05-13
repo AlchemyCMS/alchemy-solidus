35f38a3f0b156e9e1e601a3d0bc401204329f52e
rickythefox <richard@ginzburg.se>
2019-05-13 13:52:44 +0200

    Include all Spree controller helpers

--- lib/alchemy/solidus/use_solidus_layout.rb
+++ lib/alchemy/solidus/use_solidus_layout.rb
@@ -1,5 +1,11 @@
 # Include this to make Alchemy render within the Solidus layout
 Alchemy::BaseHelper.send :include, Spree::BaseHelper
 Alchemy::BaseHelper.send :include, Spree::StoreHelper
+Alchemy::BaseController.send :include, Spree::Core::ControllerHelpers::Auth
 Alchemy::BaseController.send :include, Spree::Core::ControllerHelpers::Common
+Alchemy::BaseController.send :include, Spree::Core::ControllerHelpers::Order
+Alchemy::BaseController.send :include, Spree::Core::ControllerHelpers::PaymentParameters
+Alchemy::BaseController.send :include, Spree::Core::ControllerHelpers::Pricing
+Alchemy::BaseController.send :include, Spree::Core::ControllerHelpers::Search
 Alchemy::BaseController.send :include, Spree::Core::ControllerHelpers::Store
+Alchemy::BaseController.send :include, Spree::Core::ControllerHelpers::StrongParameters
