d56a47af417a6bdea71407ed3cc24b0dfc28f847
Thomas von Deyen <thomas@vondeyen.com>
2019-11-12 18:31:12 +0100

    Refactor implementation of EssenceSpreeTaxon
    This way it passes the essence examples.

--- app/models/alchemy/essence_spree_taxon.rb
+++ app/models/alchemy/essence_spree_taxon.rb
@@ -1,14 +1,28 @@
+# frozen_string_literal: true
+
 module Alchemy
   class EssenceSpreeTaxon < ActiveRecord::Base
-    belongs_to :taxon, class_name: "Spree::Taxon", foreign_key: 'taxon_id'
+    TAXON_ID = /\A\d+\z/
 
-    acts_as_essence(
-      ingredient_column: 'taxon_id',
-      preview_text_method: 'name'
-    )
+    belongs_to :taxon, class_name: 'Spree::Taxon',
+      optional: true, foreign_key: 'taxon_id'
 
-    def ingredient
-      taxon
+    acts_as_essence(ingredient_column: :taxon)
+
+    def ingredient=(taxon_or_id)
+      case taxon_or_id
+      when TAXON_ID
+        self.taxon_id = taxon_or_id
+      when Spree::Taxon
+        self.taxon = taxon_or_id
+      else
+        super
+      end
+    end
+
+    def preview_text(_maxlength)
+      return unless taxon
+      taxon.name
     end
   end
 end
