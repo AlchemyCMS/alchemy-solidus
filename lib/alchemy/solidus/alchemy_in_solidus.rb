0d58d29f3738fab50d34de84d74fc232d3f39f84
Thomas von Deyen <thomas@vondeyen.com>
2019-12-16 14:37:49 +0100

    Move config.to_prepare block into engine
    Letting the Rails app do the reload leads to not loading
    the files during local development at all.

--- lib/alchemy/solidus/alchemy_in_solidus.rb
+++ lib/alchemy/solidus/alchemy_in_solidus.rb
@@ -1,16 +1,15 @@
-# Make sure we have everything loaded before patching classes
-Rails.application.config.to_prepare do
-  # Allows to render Alchemy content within Solidus' controller views
-  Spree::BaseController.include Alchemy::ControllerActions
-  Spree::BaseController.include Alchemy::ConfigurationMethods
+# Allows to render Alchemy content within Solidus' controller views
+Spree::StoreController.include(
+  Alchemy::ControllerActions,
+  Alchemy::ConfigurationMethods
+)
 
-  # Hook into SolidusAuthDevise controllers if present
-  if defined? Spree::Auth::Engine
-    Spree::UserPasswordsController.include Alchemy::ControllerActions
-    Spree::UserConfirmationsController.include Alchemy::ControllerActions
-    Spree::UserRegistrationsController.include Alchemy::ControllerActions
-    Spree::UserSessionsController.include Alchemy::ControllerActions
-  end
+# Hook into SolidusAuthDevise controllers if present
+if defined? Spree::Auth::Engine
+  Spree::UserPasswordsController.include Alchemy::ControllerActions
+  Spree::UserConfirmationsController.include Alchemy::ControllerActions
+  Spree::UserRegistrationsController.include Alchemy::ControllerActions
+  Spree::UserSessionsController.include Alchemy::ControllerActions
 end
 
 # Do not prefix element view partials with `spree` namespace.
