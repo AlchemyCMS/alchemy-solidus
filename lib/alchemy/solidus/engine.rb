7a1dc1591272c243d82a11f37391c2400b297da1
Thomas von Deyen <thomas@vondeyen.com>
2020-02-25 14:08:48 +0100

    Use user_class#name
    Alchemy 4.4.2 has a fix for constant lookup issues
    (https://github.com/AlchemyCMS/alchemy_cms/pull/1724/)
    
    That prefixes the user class with `::`
    
    In order to be compatible with older Alchemy versions we use
    the class#name instead.

--- lib/alchemy/solidus/engine.rb
+++ lib/alchemy/solidus/engine.rb
@@ -12,12 +12,12 @@ module Alchemy
         Alchemy.register_ability ::Spree::Ability
         ::Spree::Ability.register_ability ::Alchemy::Permissions
 
-        if Alchemy.user_class_name == 'Spree::User'
+        if Alchemy.user_class.name == 'Spree::User'
           require 'alchemy/solidus/spree_user_extension'
           Spree::User.include Alchemy::Solidus::SpreeUserExtension
         end
 
-        if Alchemy.user_class_name == 'Alchemy::User'
+        if Alchemy.user_class.name == 'Alchemy::User'
           require 'alchemy/solidus/alchemy_user_extension'
           Alchemy::User.include Alchemy::Solidus::AlchemyUserExtension
           require 'alchemy/solidus/spree_admin_unauthorized_redirect'
@@ -40,7 +40,7 @@ module Alchemy
       # Fix for +belongs_to :bill_address+ in {Spree::UserAddressBook}
       # Solidus has this set to +false+ in {Spree::Base}, but {Alchemy::User} does not inherit from it.
       initializer 'alchemy_solidus.belongs_bill_address_fix' do
-        if Alchemy.user_class_name == 'Alchemy::User'
+        if Alchemy.user_class.name == 'Alchemy::User'
           ActiveSupport.on_load(:active_record) do
             Alchemy::User.belongs_to_required_by_default = false
           end
