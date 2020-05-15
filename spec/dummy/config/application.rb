660e38ed608a4302e65d640c984f7417b49821a6
Martin Meyerhoff <mamhoff@gmail.com>
2020-05-15 17:19:24 +0200

    Update dummy app application.rb to Rails 5.2 defaults
    We support Rails 5.2 upwards.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -12,13 +12,10 @@ require "alchemy-solidus"
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1 if config.respond_to? :load_defaults
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    if config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer=)
-      config.active_record.sqlite3.represent_boolean_as_integer = true
-    end
   end
 end
