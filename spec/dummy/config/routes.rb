f07ebbac5b8472caa3d89c24bac68f9282069539
Martin Meyerhoff <mamhoff@gmail.com>
2020-05-29 12:13:56 +0200

    Mount both Alchemy and Solidus in routes.rb
    The Solidus install generator from 2.8 onwards does not insert mounting
    the Solidus engine, it only adds a comment. The Alchemy generator
    inserts this only if it's not already here. The safest option is to
    simply have all the things in our routes.rb file.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  # Let AlchemyCMS handle the root route
+  root to: 'alchemy/pages#index'
+
+  mount Spree::Core::Engine, at: "/"
+  mount Alchemy::Engine, at: '/'
 end
