23c72315208c4ab876128fe0e2267bd3f7e7f128
Thomas von Deyen <thomas@vondeyen.com>
2020-06-11 11:45:10 +0200

    Fix essence editor partials for elements w/o editor
    If an element editor is not present (default in Alchemy 5.0)
    the local variable is named after Rails defaults (the partial name).

--- spec/views/alchemy/essences/essence_spree_taxon_editor_spec.rb
+++ spec/views/alchemy/essences/essence_spree_taxon_editor_spec.rb
@@ -18,24 +18,49 @@ RSpec.describe "alchemy/essences/_essence_spree_taxon_editor" do
     allow(view).to receive(:content_label) { content.name }
   end
 
-  subject do
-    render "alchemy/essences/essence_spree_taxon_editor",
-      content: content,
-      spree: double(api_taxons_path: "/api/taxons"),
-      current_alchemy_user: double(spree_api_key: "123")
-    rendered
+  context "with local named content" do
+    subject do
+      render "alchemy/essences/essence_spree_taxon_editor",
+        content: content,
+        spree: double(api_taxons_path: "/api/taxons"),
+        current_alchemy_user: double(spree_api_key: "123")
+      rendered
+    end
+
+    it "renders a taxon input" do
+      is_expected.to have_css("input.alchemy_selectbox.full_width")
+    end
+
+    context "with a taxon related to essence" do
+      let(:taxon) { Spree::Taxon.new(id: 1) }
+      let(:essence) { Alchemy::EssenceSpreeTaxon.new(taxon_id: taxon.id) }
+
+      it "sets taxon id as value" do
+        is_expected.to have_css('input.alchemy_selectbox[value="1"]')
+      end
+    end
   end
 
-  it "renders a taxon input" do
-    is_expected.to have_css("input.alchemy_selectbox.full_width")
-  end
+  context "with local named essence_spree_taxon_editor" do
+    subject do
+      render "alchemy/essences/essence_spree_taxon_editor",
+        essence_spree_taxon_editor: content,
+        spree: double(api_taxons_path: "/api/taxons"),
+        current_alchemy_user: double(spree_api_key: "123")
+      rendered
+    end
 
-  context "with a taxon related to essence" do
-    let(:taxon) { Spree::Taxon.new(id: 1) }
-    let(:essence) { Alchemy::EssenceSpreeTaxon.new(taxon_id: taxon.id) }
+    it "renders a taxon input" do
+      is_expected.to have_css("input.alchemy_selectbox.full_width")
+    end
 
-    it "sets taxon id as value" do
-      is_expected.to have_css('input.alchemy_selectbox[value="1"]')
+    context "with a taxon related to essence" do
+      let(:taxon) { Spree::Taxon.new(id: 1) }
+      let(:essence) { Alchemy::EssenceSpreeTaxon.new(taxon_id: taxon.id) }
+
+      it "sets taxon id as value" do
+        is_expected.to have_css('input.alchemy_selectbox[value="1"]')
+      end
     end
   end
 end
--- spec/views/alchemy/essences/essence_spree_variant_editor_spec.rb
+++ spec/views/alchemy/essences/essence_spree_variant_editor_spec.rb
@@ -18,25 +18,51 @@ RSpec.describe "alchemy/essences/_essence_spree_variant_editor" do
     allow(view).to receive(:content_label) { content.name }
   end
 
-  subject do
-    render "alchemy/essences/essence_spree_variant_editor",
-      content: content,
-      spree: double(api_variants_path: "/api/variants"),
-      current_alchemy_user: double(spree_api_key: "123")
-    rendered
+  context "with local named content" do
+    subject do
+      render "alchemy/essences/essence_spree_variant_editor",
+        content: content,
+        spree: double(api_variants_path: "/api/variants"),
+        current_alchemy_user: double(spree_api_key: "123")
+      rendered
+    end
+
+    it "renders a variant input" do
+      is_expected.to have_css("input.alchemy_selectbox.full_width")
+    end
+
+    context "with a variant related to essence" do
+      let(:product) { Spree::Product.new(name: "Chocolate") }
+      let(:variant) { Spree::Variant.new(id: 1, product: product) }
+      let(:essence) { Alchemy::EssenceSpreeVariant.new(variant_id: variant.id) }
+
+      it "sets variant id as value" do
+        is_expected.to have_css('input.alchemy_selectbox[value="1"]')
+      end
+    end
   end
 
-  it "renders a variant input" do
-    is_expected.to have_css("input.alchemy_selectbox.full_width")
-  end
+  context "with local named essence_spree_variant_editor" do
+    subject do
+      render "alchemy/essences/essence_spree_variant_editor",
+        essence_spree_variant_editor: content,
+        spree: double(api_variants_path: "/api/variants"),
+        current_alchemy_user: double(spree_api_key: "123")
+      rendered
+    end
 
-  context "with a variant related to essence" do
-    let(:product) { Spree::Product.new(name: "Chocolate") }
-    let(:variant) { Spree::Variant.new(id: 1, product: product) }
-    let(:essence) { Alchemy::EssenceSpreeVariant.new(variant_id: variant.id) }
+    it "renders a variant input" do
+      is_expected.to have_css("input.alchemy_selectbox.full_width")
+    end
 
-    it "sets variant id as value" do
-      is_expected.to have_css('input.alchemy_selectbox[value="1"]')
+    context "with a variant related to essence" do
+      let(:product) { Spree::Product.new(name: "Chocolate") }
+      let(:variant) { Spree::Variant.new(id: 1, product: product) }
+      let(:essence) { Alchemy::EssenceSpreeVariant.new(variant_id: variant.id) }
+
+      it "sets variant id as value" do
+        is_expected.to have_css('input.alchemy_selectbox[value="1"]')
+      end
     end
   end
 end
