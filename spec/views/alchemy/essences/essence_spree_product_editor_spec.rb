7cc82469f0910b19c4d461da7a5d91ab98597ea9
Thomas von Deyen <thomas@vondeyen.com>
2020-06-11 11:51:44 +0200

    Format spec files with Rufo

--- spec/views/alchemy/essences/essence_spree_product_editor_spec.rb
+++ spec/views/alchemy/essences/essence_spree_product_editor_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'alchemy/essences/_essence_spree_product_editor' do
+RSpec.describe "alchemy/essences/_essence_spree_product_editor" do
   let(:content) do
     content = Alchemy::Content.new(essence: essence)
     if Alchemy.gem_version >= Gem::Version.new("4.9")
@@ -20,18 +20,18 @@ RSpec.describe 'alchemy/essences/_essence_spree_product_editor' do
   end
 
   subject do
-    render 'alchemy/essences/essence_spree_product_editor',
+    render "alchemy/essences/essence_spree_product_editor",
       content: content,
-      spree: double(api_products_path: '/api/products'),
-      current_alchemy_user: double(spree_api_key: '123')
+      spree: double(api_products_path: "/api/products"),
+      current_alchemy_user: double(spree_api_key: "123")
     rendered
   end
 
   it "renders a product input" do
-    is_expected.to have_css('input.alchemy_selectbox.full_width')
+    is_expected.to have_css("input.alchemy_selectbox.full_width")
   end
 
-  context 'with a product related to essence' do
+  context "with a product related to essence" do
     let(:product) { Spree::Product.new(id: 1) }
     let(:essence) { Alchemy::EssenceSpreeProduct.new(spree_product_id: product.id) }
 
--- spec/views/alchemy/essences/essence_spree_variant_editor_spec.rb
+++ spec/views/alchemy/essences/essence_spree_variant_editor_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'alchemy/essences/_essence_spree_variant_editor' do
+RSpec.describe "alchemy/essences/_essence_spree_variant_editor" do
   let(:content) do
     content = Alchemy::Content.new(essence: essence)
     if Alchemy.gem_version >= Gem::Version.new("4.9")
@@ -19,19 +19,19 @@ RSpec.describe 'alchemy/essences/_essence_spree_variant_editor' do
   end
 
   subject do
-    render 'alchemy/essences/essence_spree_variant_editor',
+    render "alchemy/essences/essence_spree_variant_editor",
       content: content,
-      spree: double(api_variants_path: '/api/variants'),
-      current_alchemy_user: double(spree_api_key: '123')
+      spree: double(api_variants_path: "/api/variants"),
+      current_alchemy_user: double(spree_api_key: "123")
     rendered
   end
 
   it "renders a variant input" do
-    is_expected.to have_css('input.alchemy_selectbox.full_width')
+    is_expected.to have_css("input.alchemy_selectbox.full_width")
   end
 
-  context 'with a variant related to essence' do
-    let(:product) { Spree::Product.new(name: 'Chocolate') }
+  context "with a variant related to essence" do
+    let(:product) { Spree::Product.new(name: "Chocolate") }
     let(:variant) { Spree::Variant.new(id: 1, product: product) }
     let(:essence) { Alchemy::EssenceSpreeVariant.new(variant_id: variant.id) }
 
