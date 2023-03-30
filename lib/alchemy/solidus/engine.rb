4ecafd80854df28fef4a8cce8b6d05de743702e2
Thomas von Deyen <thomas@vondeyen.com>
2023-03-30 11:13:48 +0200

    Overwrite current_spree_user if Alchemy user is used
    Solidus 3.3 introduced a breaking change how the current_spree_user
    is set. We need to overwrite the method now.

--- lib/alchemy/solidus/engine.rb
+++ lib/alchemy/solidus/engine.rb
@@ -1,36 +1,38 @@
-require 'alchemy_cms'
-require 'alchemy/version'
-require 'solidus_core'
-require 'solidus_backend'
-require 'solidus_support'
+require "alchemy_cms"
+require "alchemy/version"
+require "solidus_core"
+require "solidus_backend"
+require "solidus_support"
 
 module Alchemy
   module Solidus
     class Engine < ::Rails::Engine
-      engine_name 'alchemy_solidus'
+      engine_name "alchemy_solidus"
 
       config.to_prepare do
         Alchemy.register_ability ::Spree::Ability
         ::Spree::Ability.register_ability ::Alchemy::Permissions
 
-        if Alchemy.user_class_name == '::Spree::User'
-          require 'alchemy/solidus/spree_user_extension'
+        if Alchemy.user_class_name == "::Spree::User"
+          require "alchemy/solidus/spree_user_extension"
           Spree::User.include Alchemy::Solidus::SpreeUserExtension
         end
 
-        if Alchemy.user_class_name == '::Alchemy::User'
-          require 'alchemy/solidus/alchemy_user_extension'
+        if Alchemy.user_class_name == "::Alchemy::User"
+          require "alchemy/solidus/alchemy_user_extension"
+          require "alchemy/solidus/current_user_helpers"
           Alchemy::User.include Alchemy::Solidus::AlchemyUserExtension
-          require 'alchemy/solidus/spree_admin_unauthorized_redirect'
+          ApplicationController.include Alchemy::Solidus::CurrentUserHelpers
+          require "alchemy/solidus/spree_admin_unauthorized_redirect"
         end
 
         if SolidusSupport.frontend_available?
           # Allows to render Alchemy content within Solidus' controller views
-          require_dependency 'alchemy/solidus/alchemy_in_solidus'
+          require_dependency "alchemy/solidus/alchemy_in_solidus"
         end
 
         # Allows to use Solidus helpers within Alchemys controller views
-        require_dependency 'alchemy/solidus/use_solidus_layout'
+        require_dependency "alchemy/solidus/use_solidus_layout"
       end
     end
   end
