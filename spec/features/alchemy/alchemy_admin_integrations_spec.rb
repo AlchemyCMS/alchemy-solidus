9d3db215fa94edaab52423bbabc15e23429298d7
Thomas von Deyen <thomas@vondeyen.com>
2023-03-30 09:52:53 +0200

    Create admin user before login in admin specs
    Why has this ever worked?

--- spec/features/alchemy/alchemy_admin_integrations_spec.rb
+++ spec/features/alchemy/alchemy_admin_integrations_spec.rb
@@ -1,40 +1,39 @@
-require 'alchemy/version'
-require 'rails_helper'
+require "alchemy/version"
+require "rails_helper"
+require "alchemy/devise/test_support/factories"
 
 RSpec.feature "Admin Integration", type: :feature do
-  it 'gets redirected to login if accessing admin' do
-    visit '/admin'
+  let!(:user) { create(:alchemy_admin_user) }
 
-    expect(page).to have_field 'user_login'
+  it "gets redirected to login if accessing admin" do
+    visit "/admin"
+
+    expect(page).to have_field "user_login"
   end
 
-  it 'it is possible to login and visit Alchemy admin' do
+  it "it is possible to login and visit Alchemy admin" do
     login!
-    visit '/admin/dashboard'
+    visit "/admin/dashboard"
 
-    expect(page).to have_content 'Welcome back'
+    expect(page).to have_content "Welcome back"
   end
 
-  it 'it is possible to login and visit Spree admin' do
+  it "it is possible to login and visit Spree admin" do
     login!
-    visit '/admin/orders'
+    visit "/admin/orders"
 
-    expect(page).to have_content 'No Orders found'
+    expect(page).to have_content "No Orders found"
   end
 
   private
 
   def login!
-    visit '/admin/login'
+    visit "/admin/login"
 
-    expect(page).to have_field 'user_login'
-    fill_in 'user_login', with: 'admin'
-    fill_in 'user_password', with: 'test1234'
+    expect(page).to have_field "user_login"
+    fill_in "user_login", with: user.login
+    fill_in "user_password", with: user.password
 
-    if Gem::Version.new(Alchemy.version) >= Gem::Version.new('4.2.0')
-      click_button 'Login'
-    else
-      click_button 'login'
-    end
+    click_button "Login"
   end
 end
