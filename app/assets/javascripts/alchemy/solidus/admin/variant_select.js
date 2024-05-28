0e4d6de0e203fa0cbfa2ea07fb6b9ebea988941b
Thomas von Deyen <thomas@vondeyen.com>
2024-05-28 09:33:34 +0200

    Format variant_select with prettier

--- app/assets/javascripts/alchemy/solidus/admin/variant_select.js
+++ app/assets/javascripts/alchemy/solidus/admin/variant_select.js
@@ -1,15 +1,17 @@
 //= require alchemy/solidus/admin/select2_config
 
-$.fn.alchemyVariantSelect = function(options) {
+$.fn.alchemyVariantSelect = function (options) {
   const config = Alchemy.Solidus.getSelect2Config(options)
 
   function formatSelection(variant) {
-    return variant.options_text ? `${variant.name} - ${variant.options_text}` : variant.name
+    return variant.options_text
+      ? `${variant.name} - ${variant.options_text}`
+      : variant.name
   }
 
   function formatResult(variant) {
     return `
-      <div class="variant-select-result">  
+      <div class="variant-select-result">
         <div>
           <span>${variant.name}</span>
         </div>
@@ -17,28 +19,33 @@ $.fn.alchemyVariantSelect = function(options) {
           <span>${variant.options_text}</span>
           <span>${variant.sku}</span>
         </div>
-      </div>       
+      </div>
     `
   }
 
-  this.select2($.extend(true, config, {
-    ajax: {
-      data: function(term, page) {
-        return {
-          q: $.extend({
-            product_name_or_sku_cont: term
-          }, options.query_params),
-          page: page
-        }
+  this.select2(
+    $.extend(true, config, {
+      ajax: {
+        data: function (term, page) {
+          return {
+            q: $.extend(
+              {
+                product_name_or_sku_cont: term,
+              },
+              options.query_params
+            ),
+            page: page,
+          }
+        },
+        results: function (data, page) {
+          return {
+            results: data.variants,
+            more: page * data.per_page < data.total_count,
+          }
+        },
       },
-      results: function(data, page) {
-        return {
-          results: data.variants,
-          more: page * data.per_page < data.total_count
-        }
-      }
-    },
-    formatSelection,
-    formatResult
-  }))
+      formatSelection,
+      formatResult,
+    })
+  )
 }
