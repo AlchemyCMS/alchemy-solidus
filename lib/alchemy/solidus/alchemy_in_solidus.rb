6abcb13e23fed875583bc56604e8df4424a5fb0a
Martin Meyerhoff <mamhoff@gmail.com>
2024-11-21 13:22:45 +0100

    Move ActionView config into initializer
    We don't need to keep a whole file for a single line of ActionView
    configuration.

--- lib/alchemy/solidus/alchemy_in_solidus.rb
+++
@@ -1,3 +0,0 @@
-# Do not prefix element view partials with `spree` namespace.
-# See https://github.com/AlchemyCMS/alchemy_cms/issues/1626
-ActionView::Base.prefix_partial_path_with_controller_namespace = false
