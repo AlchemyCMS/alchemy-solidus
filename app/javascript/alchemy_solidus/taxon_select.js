dceed8cb7cf7937e014b92822f4408b30f6574f7
Thomas von Deyen <thomas@vondeyen.com>
2025-01-20 15:24:27 +0100

    Format taxon_select with prettier
    It's prettier like that

--- app/javascript/alchemy_solidus/taxon_select.js
+++ app/javascript/alchemy_solidus/taxon_select.js
@@ -1,32 +1,37 @@
 import "alchemy_solidus/select2_config"
 
-$.fn.alchemyTaxonSelect = function(options) {
+$.fn.alchemyTaxonSelect = function (options) {
   var config = Alchemy.Solidus.getSelect2Config(options)
 
-  this.select2($.extend(true, config, {
-    ajax: {
-      data: function(term, page) {
-        return {
-          q: $.extend({
-            name_cont: term
-          }, options.query_params),
-          page: page
-        }
+  this.select2(
+    $.extend(true, config, {
+      ajax: {
+        data: function (term, page) {
+          return {
+            q: $.extend(
+              {
+                name_cont: term,
+              },
+              options.query_params
+            ),
+            page: page,
+          }
+        },
+        results: function (data, page) {
+          return {
+            results: data.taxons.map(function (taxon) {
+              return {
+                id: taxon.id,
+                text: taxon.pretty_name,
+              }
+            }),
+            more: page * data.per_page < data.total_count,
+          }
+        },
       },
-      results: function(data, page) {
-        return {
-          results: data.taxons.map(function(taxon) {
-            return {
-              id: taxon.id,
-              text: taxon.pretty_name
-            }
-          }),
-          more: page * data.per_page < data.total_count
-        }
-      }
-    },
-    formatSelection: function(taxon) {
-      return taxon.text || taxon.name
-    }
-  }))
+      formatSelection: function (taxon) {
+        return taxon.text || taxon.name
+      },
+    })
+  )
 }
