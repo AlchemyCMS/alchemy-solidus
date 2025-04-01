7d7c7f22b39e841467c294d207077fa7e1802ac1
Thomas von Deyen <thomas@vondeyen.com>
2025-04-01 21:42:50 +0200

    Fix setting up dummy app for testing
    The dummy app must not have the root route defined,
    otherwise it will add a second one and any rails command
    will fail with a double root route error.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # Let AlchemyCMS handle the root route
-  root to: 'alchemy/pages#index'
-
   mount Spree::Core::Engine, at: "/"
   mount Alchemy::Engine, at: '/'
 end
