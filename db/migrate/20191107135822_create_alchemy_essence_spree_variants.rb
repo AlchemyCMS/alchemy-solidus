fca16dfc86dfa0e31d4b6fcaee370c4caae1b598
Thomas von Deyen <thomas@vondeyen.com>
2025-05-14 07:42:38 +0200

    Remove essence table migrations
    We do not use these tables anymore.

--- db/migrate/20191107135822_create_alchemy_essence_spree_variants.rb
+++
@@ -1,9 +0,0 @@
-class CreateAlchemyEssenceSpreeVariants < ActiveRecord::Migration[5.2]
-  def change
-    create_table :alchemy_essence_spree_variants do |t|
-      t.references :variant, null: true, foreign_key: {to_table: Spree::Variant.table_name}
-
-      t.timestamps
-    end
-  end
-end
